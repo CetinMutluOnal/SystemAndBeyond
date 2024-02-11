f165e2749f8bb5fe9b65e59f4187b70d6ec70e0f
CetinMutluOnal <cetinmutluonal@gmail.com>
2024-02-11 23:49:49 +0300

    Created Search components

--- server/routes/main.js
+++ server/routes/main.js
@@ -47,13 +47,46 @@ router.get('', async (req, res) => {
 router.get('/post/:id', async (req,res) => {
     try {
         const locals= {
-            title: data.title,
+            title: "System And Beyond",
             description: "Created By System And Beyond Squad."
         }
 
         let slug = req.params.id;
         const data = await Post.findById({ _id: slug })
         res.render('post', { locals,data });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+/**
+ * GET /
+ * POST - searchTerm
+ */
+
+router.post('/search', async (req,res) => {
+    try {
+        const locals= {
+            title: "Search",
+            description: "Created By System And Beyond Squad."
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[a-zA-Z0-9]/g,"")
+
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+                { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+            ]
+        });
+
+
+
+        res.render("search", {
+            data,
+            locals
+        });
     } catch (error) {
         console.log(error);
     }

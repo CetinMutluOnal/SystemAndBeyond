fb1443e65d25bb029b675987f6a1d462982466f7
CetinMutluOnal <cetinmutluonal@gmail.com>
2024-04-12 19:23:30 +0300

    Rename homepage file to home from index

--- server/routes/web/main.js
+++ server/routes/web/main.js
@@ -29,7 +29,7 @@ router.get('', async (req, res) => {
         const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
 
-        res.render('index',{
+        res.render('web/home',{
             locals,
             data,
             current: page,
